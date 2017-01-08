6ee911e11060c123d45815b5a2bbab7f523d4a62
Jim <s141587@student.hb.se>
2017-01-08 20:31:22 +0100

    df

--- GenerateDataset.py
+++ GenerateDataset.py
@@ -9,27 +9,29 @@ steamuserstats = ISteamUserStats()
 
 features = [
     'playtime_forever',
-    'achievements'
+    'achievements',
+    'number_of_games'
 ]
 
 json_file = open('steamkey.json', 'r')
 json_data = json.loads(json_file.read())
 json_file.close()
 
-df = pd.DataFrame(None, index=np.arange(len(json_data)), columns=features)
+df = pd.DataFrame(None, columns=features)
 df.index.names = ['steamID/appID']
 
 for index, id in enumerate(json_data):
-
-    games = playerserviceinfo.get_owned_games(id)['response']['games']
-    print(games)
-    for game in games:
-        war = game['appid']
-        df.set_value(id, 'playtime_forever', game['playtime_forever'])
-        achievements = steamuserstats.get_player_achievements(id, game['appid'])['playerstats']['achievements']
-        achieved = [i for i in achievements if i['achieved'] is 1]
-        df.set_value(int(str(id) + game['appid'], 'achievements', len(achieved) / len(achievements)))
-        print(game['appid'])
-    print(index, id)
+    response = playerserviceinfo.get_owned_games(id)['response']
+    if(len(response) > 1):
+        games = response['games']
+        for game in games:
+            sa = str(id) + "," + str(game['appid'])
+            df.set_value(sa, 'playtime_forever', game['playtime_forever'])
+            achievements = steamuserstats.get_player_achievements(id, game['appid'])['playerstats']['achievements']
+            print(achievements)
+            achieved = [i for i in achievements if i['achieved'] == 1]
+            df.set_value(sa, 'achievements', len(achieved) / len(achievements))
+            df.set_value(sa, 'number_of_games', response['game_count'])
+    print(df)
 
 
