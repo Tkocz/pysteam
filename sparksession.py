90a35d58deca162f599520e99222718ce6a70977
Jim <s141587@student.hb.se>
2017-02-15 11:41:00 +0100

    hej

--- sparksession.py
+++ sparksession.py
@@ -6,19 +6,15 @@ if sys.version >= '3':
     long = int
 
 from pyspark.sql import SparkSession
-
-# $example on$
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
-from pyspark.sql import Row
-# $example off$
-
 
 spark = SparkSession \
     .builder \
-    .appName("ALSExample") \
+    .appName("pysteam") \
     .getOrCreate()
 
+spark.sparkContext.setLogLevel('OFF')
 # params
 rank = 10
 nIter = 10
@@ -26,16 +22,26 @@ alpha = 40.0
 lamb = 0.01
 
 # $example on$
-ratings = spark.read.csv('Resources/formateddataset100.csv', header=True, inferSchema=True)
-print(ratings.collect())
-(training, test) = ratings.randomSplit([0.8, 0.2])
+dataset = spark.read.csv('Resources/formateddataset100.csv', header=True, inferSchema=True)
+
+(training, test) = dataset.randomSplit([0.8, 0.2])
 
 # Build the recommendation model using ALS on the training data
 als = ALS(rank= 10, maxIter=nIter, regParam=lamb, alpha=alpha, userCol="steamid", itemCol="appid", ratingCol="rating")
-model = als.fit(ratings)
+model = als.fit(dataset)
 
 predictions = model.transform(test)
-
 print(predictions.collect())
+evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction")
+rmse = evaluator.evaluate(predictions)
+print("Root-mean-square error = " + str(rmse))
+
+evaluator = RegressionEvaluator(metricName="mse", labelCol="rating", predictionCol="prediction")
+mse = evaluator.evaluate(predictions)
+print("mean-square error = " + str(mse))
+
+evaluator = RegressionEvaluator(metricName="mae", labelCol="rating", predictionCol="prediction")
+mae = evaluator.evaluate(predictions)
+print("mean absolute error = " + str(mae))
 
 spark.stop()
