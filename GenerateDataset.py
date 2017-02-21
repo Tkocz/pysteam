b6e70c3d345840523fc8d9f82e9104483cb4cc61
Martin <tkocz999@gmail.com>
2017-02-21 14:07:33 +0100

    genres for the cbf holmes

--- GenerateDataset.py
+++ GenerateDataset.py
@@ -1,16 +1,19 @@
 import json
 import pandas as pd
 import numpy as np
-from steamwebapi.api import IPlayerService, ISteamUserStats
+from steamwebapi.api import IPlayerService, ISteamUserStats, ISteamWebAPIUtil
+import steamfront
 import uuid
 playerserviceinfo = IPlayerService()
 steamuserstats = ISteamUserStats()
+client = steamfront.Client()
 
 features = [
     'steamid',
     'appid',
     'playtime_forever',
-    'achievements'
+    #'achievements',
+    'genres'
 ]
 
 
@@ -21,8 +24,8 @@ def achievementprocentage(ach):
 iddict = dict()
 
 
-#json_data = [76561198008911412, 76561198048730871, 76561198180821795]
-json_file = open('Resources/steamkey1000.json', 'r')
+#json_data = [76561198048730871, 76561198180821795, 76561198008911412]
+json_file = open('Resources/steamkey100.json', 'r')
 json_data = json.loads(json_file.read())
 json_file.close()
 
@@ -30,7 +33,10 @@ df = pd.DataFrame(None, columns=features)
 df.index.names = ['steamID/appID']
 id = -1;
 for index, steamid in enumerate(json_data):
-    response = playerserviceinfo.get_owned_games(steamid)['response']
+    try:
+        response = playerserviceinfo.get_owned_games(steamid)['response']
+    except:
+        continue
     if len(response) > 1:
         games = response['games']
         id = id + 1
@@ -38,15 +44,25 @@ for index, steamid in enumerate(json_data):
         iddict[id] = steamid
         for game in games:
             jointid = str(steamid) + "/" + str(game['appid'])
-            df.set_value(jointid, 'playtime_forever', game['playtime_forever'])
+            df.set_value(jointid, 'playtime_forever', int(game['playtime_forever']))
             df.set_value(jointid, 'steamid', int(id))
-            df.set_value(jointid, 'appid', game['appid'])
+            df.set_value(jointid, 'appid', int(game['appid']))
+
+
+            try:
+                currentGame = client.getApp(name=game['name'])
+                currentGenres = (list(currentGame.genres))
+                currentGenres.extend(list(currentGame.categories))
+                df.set_value(jointid, 'genres', currentGenres)
+            except:
+                continue
+
             #try:
             #    achievements = steamuserstats.get_player_achievements(steamid, game['appid'])['playerstats'][
             #        'achievements']
-             #   df.set_value(jointid, 'achievements', achievementprocentage(achievements))
+            #   df.set_value(jointid, 'achievements', achievementprocentage(achievements))
             #except:
-             #   df.set_value(jointid, 'achievements', None)
+            #   df.set_value(jointid, 'achievements', None)
 print(iddict)
-df.to_csv('Resources/dataset1000.csv', mode='w+')
+df.to_csv('Resources/dataset100CB.csv', mode='w+')
 
